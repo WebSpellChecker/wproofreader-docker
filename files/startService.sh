edd4bae4defef8f601f8f0e4f5163921fb8f4b72
Julia Shaptala <julia@webspellchecker.net>
2019-11-28 13:58:35 +0200

    Added comments to each step in startService.sh
    Added comments to each step in startService.sh

--- files/startService.sh
+++ files/startService.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+# obtain the name of the directory containing a script
 cd `dirname $0`
+# assign the path for a variable where AppServer is located
 appserver_path=`pwd`
+# export the libraries required for the service start
 export LD_LIBRARY_PATH=${appserver_path}/lib
+# run script to configure samples and shared dictionaries using the host name
 perl configureFiles.pl $2
+# activate a license automatically
 ./AppServerX -activateLicense $1 -y
+#service apache2 start #start Apache HTTP Server
 service apache2 start
+# start AppServer service
 ./AppServerX
