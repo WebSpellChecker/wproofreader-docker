9fba44452fb23dec139a05311b590c07b489d3fc
maxim-voronko <maxim.voronko@webspellchecker.net>
2019-12-02 17:08:55 +0200

    Activate license only if license does not exist

--- files/startService.sh
+++ files/startService.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 # change the working directory to the directory that contains the script
 cd `dirname $0`
+
 # assign the current path to a variable
 appserver_path=`pwd`
+
 # export the libraries required for the service start
 export LD_LIBRARY_PATH=${appserver_path}/lib
+
 # run script to configure samples and shared dictionaries using the host name
 perl configureFiles.pl $2
+
 # activate a license automatically
-./AppServerX -activateLicense $1 -y
+LicenseFile=/var/lib/wsc/license/license.xml
+if ! [ -f "$LicenseFile" ]; then
+   ./AppServerX -activateLicense $1 -y
+fi
+
 #start Apache HTTP Server
 service apache2 start
+
 # start AppServer service
 ./AppServerX
